75aa592dc80be19b8be991a60bca662c58a6f721
Troy Leach <troyleach29@gmail.com>
2018-03-17 19:51:59 -0500

    added Rspec and Capybara

---
+++ spec/features/pages_spec.rb
@@ -0,0 +1,25 @@
+feature 'Homepage Features' do
+  before { visit root_path }
+
+  # it won't run js code but it is fast
+  it do
+    expect(page).to have_content 'Home'
+  end
+
+  # it will run js code
+  xit '', :js do
+    expect(page).to have_content 'Hello World'
+  end
+
+  # it will open Firefox
+  # remove x from xit to run the test in Firefox on your machine to preview
+  xit '', :selenium do
+    expect(page).to have_content 'Hello World'
+  end
+
+  # it will open Chrome
+  # remove x from xit to run the test in Chrome on your machine to preview
+  xit '', :selenium_chrome do
+    expect(page).to have_content 'Hello World'
+  end
+end
