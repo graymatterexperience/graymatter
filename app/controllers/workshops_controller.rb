a31c0d376f5b3565e619fa71a31aca473156da09
Troy Leach <troyleach29@gmail.com>
2018-09-13 13:23:00 -0400

    added workshop page

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -1,5 +1,38 @@
 class WorkshopsController < ApplicationController
   def index
     @page_title = 'Workshops and Presentations'
+    @workshops = workshop_payload
   end
+
+  private
+
+  def workshop_payload
+    [
+      {
+        name: 'Workshop 1',
+        url: 'http://www.google.com',
+        logo: 'workshop.jpg',
+        description: 'I am good at containing small bits of information about this workshop.'
+      },
+      {
+        name: 'Workshop 2',
+        url: 'http://www.google.com',
+        logo: 'workshop.jpg',
+        description: 'I am good at containing small bits of information about this workshop.'
+      },
+      {
+        name: 'Workshop 3',
+        url: 'http://www.google.com',
+        logo: 'workshop.jpg',
+        description: 'I am good at containing small bits of information about this workshop.'
+      },
+      {
+        name: 'Workshop 4',
+        url: 'http://www.google.com',
+        logo: 'workshop.jpg',
+        description: 'I am good at containing small bits of information about this workshop.'
+      }
+    ]
+  end
+
 end
