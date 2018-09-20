064c2c7adac3e3151afa3eab32fb6a009502af22
Troy Leach <troyleach29@gmail.com>
2018-09-20 12:08:14 -0400

    WIP working calendar

---
+++ app/assets/javascripts/full_calendar.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  console.log('in the js file')
+  // page is now ready, initialize the calendar...
+
+  $('#calendar').fullCalendar({
+    // put your options and allbacks here
+  })
+
+});
