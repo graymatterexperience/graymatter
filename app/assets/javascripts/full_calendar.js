6b1f2d32e5164d39d142a5440e071aba02567ffa
Troy Leach <troyleach29@gmail.com>
2018-09-21 11:48:27 -0400

    finish base calendar

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -4,7 +4,18 @@ $(function() {
   // page is now ready, initialize the calendar...
 
   $('#calendar').fullCalendar({
-    events: '/events.json'
+    events: '/events.json',
+    // front the blog
+    eventRender: function (event, element) {
+      element.attr('href', 'javascript:void(0);');
+      element.click(function() {
+        $("#startTime").html(moment(event.start).format('MMM Do h:mm A'));
+        $("#endTime").html(moment(event.end).format('MMM Do h:mm A'));
+        $("#eventInfo").html(event.description);
+        $("#eventLink").attr('href', event.url);
+        $("#eventContent").dialog({ modal: true, title: event.title, width:350});
+      });
+    }
     // put your options and allbacks here
   })
 
