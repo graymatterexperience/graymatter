7f26d23ee1c64fbb50352cbd7f4fe5a13f969fa3
Troy Leach <troyleach29@gmail.com>
2018-10-02 20:20:55 -0400

    WIP finished student filter

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,12 +1,19 @@
+function clear_filter() {
+  document.getElementById('clear-filter-chip').style.display = 'none';
+  location.reload();
+}
+
 function toggle_cohort_div(id) {
+  document.getElementById('clear-filter-chip').style.display = 'inline-block';
   var divelement = document.getElementById(id);
-  var dividerLine = document.getElementById('divider-line')
   if (divelement.style.display == 'none') {
     divelement.style.display = 'table';
-    dividerLine.style.display = 'block'
   } else {
     divelement.style.display = 'none';
-    dividerLine.style.display = 'none'
   }
 
 }
+
+$(document).ready(function(){
+  $('.collapsible').collapsible();
+})
