6e7ef0b242dab543026ed54088f590c9868da471
Troy Leach <troyleach29@gmail.com>
2018-10-04 16:15:16 -0400

    WIP added the front end for mentors

--- app/controllers/mentors_controller.rb
+++ app/controllers/mentors_controller.rb
@@ -1,22 +1,6 @@
 class MentorsController < ApplicationController
   def index
     @page_title = 'Mentors'
-    @mentors = 15.times.map { |_| mentors_payload }
-    # @mentors = nil
+    @mentors = User.all.select(&:mentor?)
   end
-
-
-  private
-  
-    def mentors_payload
-      {
-        "avatar": "/assets/user.jpg",   #will change this once we have the API up... logic will be user avatar or stock avatar
-        "name": Faker::Name.name,
-        "company": Faker::Company.name,
-        "specialty": Faker::Job.field,
-        "email": Faker::Internet.email,
-        "phone": Faker::PhoneNumber.cell_phone,
-        "linkedin": "https://www.linkedin.com/company/the-gray-matter-experience/"
-      }.as_json
-    end
 end
