aa68d6f48f1a8ea1cacb7d3a085217f1e16ac961
Troy Leach <troyleach29@gmail.com>
2018-10-17 13:04:12 -0400

    added the generate password method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  require 'securerandom'
+  def generate_password
+    SecureRandom.hex(4)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,9 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
                     format: {with: VALID_EMAIL_REGEX},
                     uniqueness: {case_sensitive: false}
-  validates :password, presence: true, length: { minimum: 6 }, on: :create
+# TODO the way I am creating passwords, I will need to validate this a different way
+# or figure out a work around for the archive_user
+  # validates :password, presence: true, length: { minimum: 6 }, on: :update
 
   has_many :posts, class_name: 'Post', foreign_key: :auther_id, primary_key: :id
   has_and_belongs_to_many :cohorts
