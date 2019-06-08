e822fc7dc183f7e2d16458affe1f2446dcf4c886
Troy Leach <troyleach29@gmail.com>
2019-06-08 11:28:38 -0500

    fix broken test

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+include SignIn
 
 RSpec.describe GroupsController, type: :controller do
   describe 'GET #index' do
     it 'returns http success' do
+      user = create(:admin_user)
+      sign_in_user(user)
       get :index
       expect(response).to have_http_status(:success)
     end
